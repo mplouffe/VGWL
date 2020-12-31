c6378fd3c9dfc8f2f4aaa1338c1626b01fcb9bf2
mplouffe <matheuplouffe@gmail.com>
2020-12-31 05:07:59 -0600

    Added script to transition between ragdoll and animated controlled.

---
+++ VideoGameWrestlingLeague/Assets/Scripts/RagdollScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//A very simple script to show test the RagdollHelper
+
+
+public class RagdollScript : MonoBehaviour {
+
+    private RagdollHelper helper;
+	void Start () {
+
+        helper = GetComponent<RagdollHelper>();
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+            helper.Ragdolled = true;
+		}
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            helper.Ragdolled = false;
+        }
+	}
+}
