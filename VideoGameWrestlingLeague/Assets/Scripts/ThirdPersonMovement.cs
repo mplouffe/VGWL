71d32a884cd4639e57203deb85a3f101d2e4d114
mplouffe <matheuplouffe@gmail.com>
2020-12-31 21:43:12 -0600

    Started working on basic third person controller.

---
+++ VideoGameWrestlingLeague/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform target;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    float gravity = 0f;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        gravity -= 9.81f * Time.deltaTime;
+        Vector3 direction = new Vector3(horizontal, gravity, vertical).normalized;
+        Vector3 vectorToTarget = (target.position - transform.position).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(vectorToTarget.x, vectorToTarget.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+
+        if (controller.isGrounded)
+        {
+            gravity = 0;
+        }
+    }
+}
